11409a5b5d03bcbe680b9b3534f5d3a4a57749b6
emab <emab@odoo.com>
2021-11-23 10:47:22 -0600

    Added files

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Layouts',
+    'summary': """Load custom layouts to modify a Studio Header/Footer.""",
+    'description':"""
+        - New field on the product (conversion_rate) and new price_per_liter on some models (stored in sales and purchase)
+        - Quadrigram: emab.
+        """,
+    'author': 'Odoo, Inc.',
+    'website': 'https://www.odoo.com',
+    'category':'Custom Development',
+    'version': '0.1',
+    'depends':[],
+    'data':[
+        'views/custom_layouts.xml'
+    ],
+    'licence': 'OPL-1'
+}
